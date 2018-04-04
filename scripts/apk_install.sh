8519cff2b94a93b01fdaa37ad8e8b8e8afd46270
Satohiro Wakabayashi <wakabayashi@twogate.com>
2018-04-05 02:32:36 +0900

    Add Dockerfile

---
+++ scripts/apk_install.sh
@@ -0,0 +1,12 @@
+# ==============================================================================
+# scripts - apk install
+# ==============================================================================
+apk --update --no-cache add \
+  alpine-sdk \
+  libxml2-dev \
+  libxslt-dev \
+  postgresql-client \
+  postgresql-dev \
+  nodejs \
+  tzdata \
+  less
